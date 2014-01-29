d0360b6063797e9feb7ae5bb5fd9098e2036c001
zhongzf <zhongzf@126.com>
2014-01-30 01:28:34 +0800

    Orchard.sdf and Orchard.sql

--- src/Orchard.Web/Modules/Orchard.Security/Services/RolesBasedAuthorizationService.cs
+++ src/Orchard.Web/Modules/Orchard.Security/Services/RolesBasedAuthorizationService.cs
@@ -1,4 +1,6 @@
-﻿using Orchard.Security;
+﻿using Orchard.Localization;
+using Orchard.Logging;
+using Orchard.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,27 @@ namespace Orchard.Security.Services
 {
     public class RolesBasedAuthorizationService : IAuthorizationService
     {
+        public RolesBasedAuthorizationService()
+        {
+            T = NullLocalizer.Instance;
+            Logger = NullLogger.Instance;
+        }
+
+        public Localizer T { get; set; }
+        public ILogger Logger { get; set; }
+
+
         public void CheckAccess(Orchard.Security.Permissions.Permission permission, IUser user, ContentManagement.IContent content)
         {
+            if (!TryCheckAccess(permission, user, content))
+            {
+                throw new OrchardSecurityException(T("A security exception occurred in the content management system."))
+                {
+                    PermissionName = permission.Name,
+                    User = user,
+                    Content = content
+                };
+            }
         }
 
         public bool TryCheckAccess(Orchard.Security.Permissions.Permission permission, IUser user, ContentManagement.IContent content)
